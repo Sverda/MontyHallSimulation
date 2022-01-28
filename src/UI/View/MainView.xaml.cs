eb90ce8565fc6fa98906d51070bb364b8e07b452
Damian Ubowski <damian.ubowski.0@gmail.com>
2022-01-29 00:30:13 +0100

    Move main menu to user control

--- src/UI/View/MainView.xaml.cs
+++ src/UI/View/MainView.xaml.cs
@@ -12,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using UI.View;
 
-namespace UI
+namespace UI.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : UserControl
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
-            main.Content = new MainView();
         }
     }
 }
